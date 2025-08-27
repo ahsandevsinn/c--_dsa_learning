4804958552bae6fd51e446061414eaa4aed3767f
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-08-27 07:15:33 +0500

    data types

--- code.cpp
+++ code.cpp
@@ -3,7 +3,18 @@ using namespace std;
 // this is the starting point of execution
 int main(int argc, char const *argv[])
 {
-    cout << "Ahsan" "\n" << "Khan" << endl;
+    int age = 10; // 4 bytes
+    const char* grade = "A";; // 1 byte
+    float PI = 3.14f;  // 4 bytes
+    bool isSafe = true; // 1 byte
+    double height = 2.90; // 8 bytes
+
+    cout << age << endl;
+    cout << grade << endl;
+    cout << PI << endl;
+    cout << isSafe << endl;
+    cout << height << endl;
+
     return 0;
 }
 // this is a boiler code
