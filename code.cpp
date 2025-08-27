373f42ccad4af49dbde097a993b1c6afd1ec3776
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-08-27 06:14:51 +0500

    comments and boiler code

--- code.cpp
+++ code.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
+// this is the starting point of execution
 int main(int argc, char const *argv[])
 {
     cout << "Ahsan" "\n" << "Khan" << endl;
     return 0;
 }
+// this is a boiler code
