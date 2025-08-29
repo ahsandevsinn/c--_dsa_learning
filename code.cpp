4d0239fcf7751dd9a7dad0c89a082510f7c27b2b
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-08-29 08:14:44 +0500

    type casting

--- code.cpp
+++ code.cpp
@@ -3,17 +3,26 @@ using namespace std;
 // this is the starting point of execution
 int main(int argc, char const *argv[])
 {
-    int age = 10; // 4 bytes
-    const char* grade = "A";; // 1 byte
-    float PI = 3.14f;  // 4 bytes
-    bool isSafe = true; // 1 byte
-    double height = 2.90; // 8 bytes
+    // int age = 10; // 4 bytes
+    // const char* grade = "A";; // 1 byte
+    // float PI = 3.14f;  // 4 bytes
+    // bool isSafe = true; // 1 byte
+    // double height = 2.90; // 8 bytes
 
-    cout << age << endl;
-    cout << grade << endl;
-    cout << PI << endl;
-    cout << isSafe << endl;
-    cout << height << endl;
+    // cout << age << endl;
+    // cout << grade << endl;
+    // cout << PI << endl;
+    // cout << isSafe << endl;
+    // cout << height << endl;
+    float a  = 3.14f;
+    double value = a;
+    cout << value << endl;
+    double b = 100.99;
+    int bValue = (int)b;
+     
+    cout << bValue << endl;
+
+
 
     return 0;
 }
